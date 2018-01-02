23585cf67759cada38cc4d87a1cf56d7cee6ff3d
Jaya Gupta <mail2me17@gmail.com>
2018-01-01 22:31:11 -0800

    app change

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 
 var express = require('express');
 var path = require( 'path');
-//var favicon = require('serve-favicon');
 var logger =require('morgan');
 var cookieParser =require('cookie-parser');
 var bodyParser =require('body-parser');
@@ -33,19 +32,6 @@ let app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// var port = process.env.PORT || 5000;
-//
-// app.set('port',(process.env.PORT || 5000));
-//
-// app.listen(port,"0.0.0.0",function () {
-//     console.log("Listening on Port 5000");
-// });
-
-
-
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
