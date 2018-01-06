61f68308f965d3fcf37488055004b34580ba0dbe
Jaya Gupta <mail2me17@gmail.com>
2018-01-05 21:24:08 -0800

    find

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -11,72 +11,20 @@ bookRouter.use(bodyParser.json());
 bookRouter.route('/')
     .get(function (req,res,next) {
 
-        if(req.query.filterValue != undefined){
+        Books.find({},function (err,book) {
 
-            let fieldValue = req.query.filterValue;
+            if (err) throw err;
 
-            let arr = [];
-
-            let regexValue= fieldValue.replace(/'/g,'') +'.*';
-
-            // console.log("\""+ regexValue +"\"");
-
-            let regExp = {};
-
-            regExp["$regex"] = '"'+ regexValue + '"';
-            regExp["$options"] = "i";
-
-            let f1 = JSON.parse(JSON.stringify( {"title" : regExp}));
-            let f2 = JSON.stringify( {"author" : regExp}).replace(/'/g,'');
-            let f3 = JSON.stringify( {"publication" : regExp}).replace(/'/g,'');
-
-            arr.push(f1);
-            arr.push(f2);
-            arr.push(f3);
-
-            console.log(f1);
-
-            let query = {};
-
-            query["$or"] = arr;
-
-            console.log(query);
-
-            //let tempQuery = query;
-
-            //console.log(temp);
-
-            Books.find(query,function (err,book) {
-
-                if (err) throw err;
-
-                res.set({
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "OPTIONS, GET, POST, DELETE, PUT",
-                    'Access-Control-Allow-Headers': 'Content-Type'
-                });
-
-                    console.log(book);
-                    res.json(book);
+            res.set({
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS, GET, POST, DELETE, PUT",
+                'Access-Control-Allow-Headers': 'Content-Type'
             });
-        }
-        else{
-            Books.find({},function (err,book) {
 
-                if (err) throw err;
+            console.log(book);
 
-                res.set({
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "OPTIONS, GET, POST, DELETE, PUT",
-                    'Access-Control-Allow-Headers': 'Content-Type'
-                });
-
-                console.log(book);
-
-                res.json(book);
-            });
-        }
-
+            res.json(book);
+        });
     })
     .post(function (req,res,next) {
         Books.create(req.body,function(err, book){
