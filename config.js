06f028f8a3a363c7f2172818bd6361ab42efaaf8
Jaya Gupta <mail2me17@gmail.com>
2018-02-05 10:47:27 -0800

    config change

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
 
     'secretKey' : '12345-67890-09876-54321',
     //'mongoUrl': 'mongodb://localhost:27017/ELibrary'
-    'mongoUrl': 'mongodb://guptajaya496:guptajaya496#@ds131237.mlab.com:31237/booksrfriends'
+    'mongoUrl': 'mongodb://guptajaya496:guptajaya496@ds131237.mlab.com:31237/booksrfriends'
 };
